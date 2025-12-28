50e11901942d0080c8d552f0ad31ac591e81a0fa
Aditya2012-22 <adi.adi22b@gmail.com>
2025-12-28 16:33:27 +0530

    initial commit

---
+++ number swapping.py
@@ -0,0 +1,11 @@
+x = input("Enter value of x:")
+y = input("Enter value of y:")
+
+#Swapping
+temp = x
+x = y
+y = temp
+
+#Displaying results after swapping
+print("value of x after swapping:", x)
+print("value of y afetr swaping:", y)
---
+++ string operators.py
@@ -0,0 +1,15 @@
+first_name = "Codingal"
+last_name = "Educations"
+full_name = first_name +last_name
+example = "Haa"*5
+
+print("First Name:", first_name)
+print("Last Name:", last_name)
+print("Full Name:", full_name)
+print("String multiplied 5 times gives this result:", example)
+
+word = 'Codingal'
+print("Length of string:", len(word))
+print("First letter of string:", word[0])
+print("Last letter of string:", word[5])
+print("String Sliced:", word[0:3])
